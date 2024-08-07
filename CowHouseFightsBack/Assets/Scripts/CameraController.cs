d49f7c7a726482902db0f88dc0173f3c4a109c9f
Andrejs/daviskyLV <ledaviskylv@gmail.com>
2024-08-07 03:29:45 +0200

    Simple camera movement

---
+++ CowHouseFightsBack/Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private float cameraSpeed;
+    
+    private PlayerControls controls;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+    
+    public void LateUpdate()
+    {
+        var scrollDirection = controls.Playfield.Movement.ReadValue<Vector2>().x * cameraSpeed;
+        transform.position += new Vector3(0, 0, scrollDirection * Time.deltaTime);
+    }
+}
