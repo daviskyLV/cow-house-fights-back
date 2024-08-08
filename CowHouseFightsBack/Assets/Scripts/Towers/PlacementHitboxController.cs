3df95677a9e1920e3278f92f8374da7330c09c7e
Andrejs/daviskyLV <ledaviskylv@gmail.com>
2024-08-08 23:24:42 +0200

    Chicken tower base

---
+++ CowHouseFightsBack/Assets/Scripts/Towers/PlacementHitboxController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementHitboxController : MonoBehaviour
+{
+    [SerializeField] private Material noCollisionColor;
+
+    [SerializeField] private Material collisionColor;
+
+    private List<Transform> hitboxes;
+    /// <summary>
+    /// Whenever a tower's placement availability changes. Boolean for can/can't be placed
+    /// </summary>
+    public event Action<bool> OnPlacementAvailable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hitboxes = new List<Transform>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            hitboxes.Add(transform.GetChild(i));
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        OnPlacementAvailable?.Invoke(false);
+        foreach (var t in hitboxes)
+        {
+            var mr = t.GetComponent<MeshRenderer>();
+            mr.material = collisionColor;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        OnPlacementAvailable?.Invoke(true);
+        foreach (var t in hitboxes)
+        {
+            var mr = t.GetComponent<MeshRenderer>();
+            mr.material = noCollisionColor;
+        }
+    }
+}
